4daaf4ef5fe40645f03bcac1df04707cac80fe9e
Charles Southerland <charlie@stuphlabs.com>
2013-03-11 23:40:28 -0500

    Should be working, but not very thoroughly tested yet.

---
+++ test.c
@@ -0,0 +1,47 @@
+#include "base64.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <dejagnu.h>
+
+int main()
+{
+	char* msg = "Hello there, base64 encoder!\n";
+	size_t len = strlen(msg);
+	size_t enclen = 0;
+	size_t declen = 0;
+	size_t i = 0;
+	char* encmsg;
+	char* decmsg;
+
+	note("Message is: \"%s\"", msg);
+
+	encmsg = base64_encode(&enclen, len, msg);
+	if (encmsg == NULL) {
+		fail("Encoded message was NULL");
+	} else if (enclen <= len) {
+		fail("Encoded message was too short");
+	} else if (strnlen(encmsg, BUFSIZ) > BUFSIZ - 1) {
+		fail("No terminating NULL in encoded message.");
+	} else {
+		pass("Encoded message seems plausible: \"%s\"", encmsg);
+	}
+
+	decmsg = base64_decode(&declen, enclen, encmsg);
+	if (decmsg == NULL) {
+		fail("Decoded message was NULL");
+	} else if (declen >= enclen) {
+		fail("Decoded message was too long for the encoded message");
+	} else if (!strnlen(decmsg, BUFSIZ) > BUFSIZ - 1) {
+		fail("No terminating NULL in decoded message.");
+	} else if (strncmp(msg, decmsg, BUFSIZ) != 0) {
+		fail("Decoded message does not match: \"%s\"", decmsg);
+	} else {
+		pass("Encoded message matches: \"%s\"", decmsg);
+	}
+
+	totals();
+	return 0;
+}
+
+
